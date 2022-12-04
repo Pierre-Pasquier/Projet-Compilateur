abafb3731db95691c7510523a2b767a743d83842
IoriGingembre Nathan <nathan.iori-gingembre@telecomnancy.eu>
2022-12-04 08:54:32 +0000

    Upload New File

---
+++ src/ast/For.java
@@ -0,0 +1,25 @@
+package ast;
+
+public class For implements Ast {
+
+    public <T> T accept(AstVisitor<T> visitor){
+        return visitor.visit(this);
+    }
+
+    public Ast deb;
+    public Ast fin;
+    public Ast faire;
+    public Ast idf;
+
+
+
+    public For(Ast deb,Ast fin,Ast faire,Ast idf){
+        this.deb=deb;
+        this.fin=fin;
+        this.faire=faire;
+        this.idf=idf;
+
+    }
+
+    
+}
