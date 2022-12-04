7c36a617a04b14fb401b9cdef127dd3a08a31e0c
IoriGingembre Nathan <nathan.iori-gingembre@telecomnancy.eu>
2022-12-04 08:56:36 +0000

    Upload New File

---
+++ src/ast/Printe.java
@@ -0,0 +1,15 @@
+package ast;
+
+public class Printe implements Ast {
+
+    public <T> T accept(AstVisitor<T> visitor){
+        return visitor.visit(this);
+    }
+
+    public Ast printe;
+
+
+    public Printe(Ast printe){
+        this.printe = printe;
+    }
+}
