5ccee3651806d6c9a0656f70319caaf222ee46ac
IoriGingembre Nathan <nathan.iori-gingembre@telecomnancy.eu>
2022-12-04 08:53:49 +0000

    Upload New File

---
+++ src/ast/Declarationlists.java
@@ -0,0 +1,18 @@
+package ast;
+
+public class Declarationlists implements Ast {
+
+    public <T> T accept(AstVisitor<T> visitor){
+        return visitor.visit(this);
+    }
+
+    public Ast affect;
+    public Ast dans;
+
+
+    public Declarationlists(Ast dans,Ast affect){
+        this.dans=dans;
+        this.affect=affect;
+    }
+  
+}
