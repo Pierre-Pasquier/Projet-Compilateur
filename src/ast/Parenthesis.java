ef60764a6a5ee0d042a5a16e3c5b964ee45a9283
IoriGingembre Nathan <nathan.iori-gingembre@telecomnancy.eu>
2022-12-04 08:56:13 +0000

    Upload New File

---
+++ src/ast/Parenthesis.java
@@ -0,0 +1,16 @@
+package ast;
+
+public class Parenthesis implements Ast {
+
+    public <T> T accept(AstVisitor<T> visitor){
+        return visitor.visit(this);
+    }
+
+    public Ast parenthesis;
+
+
+    public Parenthesis(Ast parenthesis){
+        this.parenthesis = parenthesis;
+    }
+}
+
