31fe16b470f5372707e257fa8c2ddfab84067e57
IoriGingembre Nathan <nathan.iori-gingembre@telecomnancy.eu>
2022-12-04 08:57:18 +0000

    Upload New File

---
+++ src/ast/While.java
@@ -0,0 +1,20 @@
+package ast;
+
+public class While implements Ast {
+
+    public <T> T accept(AstVisitor<T> visitor){
+        return visitor.visit(this);
+    }
+
+    public Ast condition;
+    public Ast faire;
+
+
+    public While(Ast condition,Ast faire){
+        this.condition = condition;
+        this.faire = faire;
+
+
+    }
+}
+
