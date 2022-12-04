5695ddab32386a4983e7947adc067ec286d3067a
IoriGingembre Nathan <nathan.iori-gingembre@telecomnancy.eu>
2022-12-04 08:55:44 +0000

    Upload New File

---
+++ src/ast/Nilop.java
@@ -0,0 +1,15 @@
+package ast;
+
+public class Nilop implements Ast {
+
+    public <T> T accept(AstVisitor<T> visitor){
+        return visitor.visit(this);
+    }
+
+    public Ast nilop;
+
+
+    public Nilop(Ast nilop){
+        this.nilop = nilop;
+    }
+}
