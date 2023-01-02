a7072b45bbd104580216a35bb83f5eb86224ec28
IoriGingembre Nathan <nathan.iori-gingembre@telecomnancy.eu>
2023-01-02 11:26:45 +0000

    Upload New File

---
+++ src/ast/Do.java
@@ -0,0 +1,12 @@
+package ast;
+
+public class Do implements Ast{
+    public <T> T accept(AstVisitor<T> visitor){
+        return visitor.visit(this);
+    }
+    
+    public Ast faire;
+    public Do(Ast faire){
+        this.faire=faire;
+    }
+}
