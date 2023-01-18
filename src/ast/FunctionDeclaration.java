f9675e4aea4e92750402383017cea5f28ecc5824
Iori-Gingembre <macnathan@MacBook-Pro-de-Iori-Gingembre.local>
2023-01-18 19:15:09 +0100

    replacement redondance arg fonction

--- src/ast/FunctionDeclaration.java
+++ src/ast/FunctionDeclaration.java
@@ -33,9 +33,22 @@ public class FunctionDeclaration implements Ast {
         String name = Idf.ControleSemantique().get(1);
         List<String> typf = typefields.ControleSemantique();
         String type = TDS.getType(name, tds);
-        System.out.println("Classe de expr : " + expr.getClass().getName());
-        System.out.println("type de retour de la fonction " + name + " : " + type);
-        System.out.println(exp);
+
+        for(int i=0;i<typf.size()/4;i++){
+            List<String> listel = new ArrayList<>();
+            if(typf.get(i*4)!=null && typf.get((i*4)+3)!=null){
+                listel.add(typf.get(i*4));
+                listel.add(typf.get((i*4)+3));
+                for(int k=i+1;k<typf.size()/4;k++){
+                    if(listel.get(0)!=null && listel.get(1)!=null && listel.get(0).compareTo(typf.get(k*4))==0 && listel.get(1).compareTo(typf.get((k*4)+3))==0 ){
+                        TDS.write("Erreur ligne " + line + " : plusieurs arguments ont le nom " + listel.get(1)+ " et le même type "+ listel.get(0));
+                    }
+                }
+            }
+        }
+        //System.out.println("Classe de expr : " + expr.getClass().getName());
+        //System.out.println("type de retour de la fonction " + name + " : " + type);
+        //System.out.println(exp);
         //boucle sur les élément de expr et on vérifie si ils sont tous égaux ou non
         for (int i = 0; i < exp.size()/2; i++) {
             if (exp.get(2*i).equals(type) || exp.get(2*i).equals("")){
